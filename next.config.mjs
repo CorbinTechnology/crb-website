84607683507f5ef85778dd41660672f05f5f973e
CorbinTechnology <techwork@corbintech.co.in>
2024-03-11 16:17:41 +0530

    created project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    pageExtensions: ["ts", "tsx"], 
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/home",
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  
+  export default nextConfig;
